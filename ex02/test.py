7cf6f7a469698e8082a7a334f6079882f83cb79c
Lala_pukuchan <ruruover1105@gmail.com>
2023-12-19 12:00:31 +0100

    ex02

--- ex02/test.py
+++ ex02/test.py
@@ -3,11 +3,15 @@ from loss import loss_
 
 X = np.array([0, 15, -9, 7, 12, 3, -21]).reshape((-1, 1))
 Y = np.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
+print("---ex.1---")
 # Example 1:
-print(loss_(X, Y))
+print("loss_: \n", loss_(X, Y))
 # Output:
+print("expected: \n", 2.142857142857143)
 # 2.142857142857143
+print("\n---ex.2---")
 # Example 2:
-print(loss_(X, X))
+print("loss_: \n", loss_(X, X))
+print("expected: \n", 0.0)
 # Output:
 # 0.0
