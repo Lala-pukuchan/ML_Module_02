4ee1548670eeb431ebba239175fc91aa3d254b57
Lala_pukuchan <ruruover1105@gmail.com>
2023-12-21 08:49:57 +0100

    ex07

--- ex07/test.py
+++ ex07/test.py
@@ -2,6 +2,7 @@ import numpy as np
 from ex07.polynomial_model import add_polynomial_features
 
 x = np.arange(1, 6).reshape(-1, 1)
+print("---Example 0---")
 # Example 0:
 print("my polynominal: \n", add_polynomial_features(x, 3))
 # Output:
@@ -9,6 +10,7 @@ print(
     "expected polynominal: \n",
     np.array([[1, 1, 1], [2, 4, 8], [3, 9, 27], [4, 16, 64], [5, 25, 125]]),
 )
+print("\n---Example 1---")
 # Example 1:
 print("my polynominal: \n", add_polynomial_features(x, 6))
 # Output:
