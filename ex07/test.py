d36524604c96b6e69bca603d64ef05571a07f262
Lala_pukuchan <ruruover1105@gmail.com>
2023-12-21 09:56:50 +0100

    ex07

--- ex07/test.py
+++ ex07/test.py
@@ -1,5 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from ex07.polynomial_model import add_polynomial_features
+from ex05.mylinearregression import MyLinearRegression as MyLR
 
 x = np.arange(1, 6).reshape(-1, 1)
 print("---Example 0---")
@@ -26,3 +28,35 @@ print(
         ]
     ),
 )
+
+x = np.arange(1,11).reshape(-1,1)
+y = np.array([[ 1.39270298],
+[ 3.88237651],
+[ 4.37726357],
+[ 4.63389049],
+[ 7.79814439],
+[ 6.41717461],
+[ 8.63429886],
+[ 8.19939795],
+[10.37567392],
+[10.68238222]])
+plt.scatter(x,y)
+plt.savefig("results/ex07/result_ex07_figure-1.png")
+plt.close()
+
+# Build the model:
+x_ = add_polynomial_features(x, 3)
+theta4 = np.array([[1], [1], [1], [1]]).reshape(-1, 1)
+my_lr = MyLR(theta4, alpha=1e-6, max_iter=600000)
+print("theta4 before:", theta4)
+my_lr.fit_(x_, y)
+print("theta4 after:", my_lr.theta)
+# Plot:
+## To get a smooth curve, we need a lot of data points
+continuous_x = np.arange(1,10.01, 0.01).reshape(-1,1)
+continuous_x_ = add_polynomial_features(continuous_x, 3)
+y_hat = my_lr.predict_(continuous_x_)
+plt.scatter(x,y)
+plt.plot(continuous_x, y_hat, color='orange')
+plt.savefig("results/ex07/result_ex07_figure-2.png")
+plt.close()
