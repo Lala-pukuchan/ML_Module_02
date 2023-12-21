68b56dec9e37ac9dbdc16269f900b8bf7c47780b
Lala_pukuchan <ruruover1105@gmail.com>
2023-12-21 10:49:05 +0100

    ex09

--- ex09/test.py
+++ ex09/test.py
@@ -4,38 +4,20 @@ from ex09.data_spliter import data_spliter
 x1 = np.array([1, 42, 300, 10, 59]).reshape((-1, 1))
 y = np.array([0, 1, 0, 1, 0]).reshape((-1, 1))
 
-print("Example 1:")
+print("\nExample 1:")
 # Example 1:
-print(data_spliter(x1, y, 0.8))
-# Output:
-# (array([ 1, 59, 42, 300]), array([10]), array([0, 0, 1, 0]), array([1]))
+print("my data spliter\n", data_spliter(x1, y, 0.8))
 
-print("Example 2:")
+print("\nExample 2:")
 # Example 2:
-print(data_spliter(x1, y, 0.5))
-# Output:
-# (array([59, 10]), array([ 1, 300, 42]), array([0, 1]), array([0, 0, 1]))
+print("my data spliter\n", data_spliter(x1, y, 0.5))
 x2 = np.array([[1, 42], [300, 10], [59, 1], [300, 59], [10, 42]])
 y = np.array([0, 1, 0, 1, 0]).reshape((-1, 1))
 
-print("Example 3:")
+print("\nExample 3:")
 # Example 3:
-print(data_spliter(x2, y, 0.8))
-# Output:
-# (
-#    array([[10, 42], [300, 59], [59, 1], [300, 10]]),
-#    array([[1, 42]]),
-#    array([0, 1, 0, 1]),
-#    array([0]),
-# )
+print("my data spliter\n", data_spliter(x2, y, 0.8))
 
-print("Example 4:")
+print("\nExample 4:")
 # Example 4:
-print(data_spliter(x2, y, 0.5))
-# Output:
-# (
-#    array([[59, 1], [10, 42]]),
-#    array([[300, 10], [300, 59], [1, 42]]),
-#    array([0, 0]),
-#    array([1, 1, 0]),
-# )
+print("my data spliter\n", data_spliter(x2, y, 0.5))
